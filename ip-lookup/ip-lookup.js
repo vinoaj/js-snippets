887545197365857edc062984e4623ad860e44a14
Vinoaj Vijeyakumaar <vinoaj@gmail.com>
2017-11-05 15:13:36 +1100

    cleanup

--- ip-lookup/ip-lookup.js
+++ ip-lookup/ip-lookup.js
@@ -5,16 +5,31 @@
 
  if (window.jQuery === undefined) {
     var s = document.createElement('script');
-    s.src = "//code.jquery.com/jquery-3.2.1.min.js";
+    s.src = "https://code.jquery.com/jquery-3.2.1.min.js";
     document.head.appendChild(s);
 }
 
 var ipLookup = {};
-ipLookup.lookupData = {};
+
+/**
+ * Sample output
+ * city: "Forresters Beach",
+ * country_code: "AU",
+ * country_name: "Australia",
+ * ip: "61.68.92.66",
+ * latitude: -33.407,
+ * longitude: 151.476,
+ * metro_code: 0,
+ * region_code: "NSW",
+ * region_name: "New South Wales",
+ * time_zone: "Australia/Sydney",
+ * zip_code: "2260"
+ **/
+ipLookup.data = {};
 ipLookup.promises = {};
 
 ipLookup.promises.geoIpCall = new Promise(function(resolve, reject) {
-  $.getJSON('//freegeoip.net/json/?callback=?').done(function(data) {
+  $.getJSON('https://freegeoip.net/json/?callback=?').done(function(data) {
       resolve(data);
   }).fail(function(data) {
       reject(data);
@@ -24,10 +39,10 @@ ipLookup.promises.geoIpCall = new Promise(function(resolve, reject) {
 ipLookup.lookup = function () {
   return new Promise(function(resolve,reject) {
     ipLookup.promises.geoIpCall.then(function(data) {
-      ipLookup.lookupData = data;
+      ipLookup.data = data;
       resolve(data);
     }, function(error) {
-      reject(data);
+      reject(error);
     });
   });
 };
