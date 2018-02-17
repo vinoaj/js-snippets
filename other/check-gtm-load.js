6c7cd86ab82760da1270b204b8b807efc57206be
Vinoaj Vijeyakumaar <vinoaj@gmail.com>
2018-02-17 17:27:39 +1100

    Introduced other/

---
+++ other/check-gtm-load.js
@@ -0,0 +1,38 @@
+/** 
+ * Code to insert into an HTML document (preferably in <head>) to determine 
+ *   whether Google Tag Manager has loaded or not. If it's not loaded, a
+ *   HTTP endpoint is contacted to inform it of this.
+ */
+
+function checkGTMLoad() {
+    if (!window['google_tag_manager']) {
+        console.log('GTM did not load');
+        payload = {
+            tid: 'UA-108507372-1',
+            dp: '/GTM_not_fired'
+        };
+
+        var endpoint = 'https://us-central1-phrasal-charger-185822.cloudfunctions.net/relayMPHit';
+        var httpRequest = new XMLHttpRequest();
+        httpRequest.open('POST', endpoint);
+        httpRequest.setRequestHeader('Content-Type', 'application/json');
+        httpRequest.send(JSON.stringify(payload));
+    } else {
+        //Do something here
+        console.log('GTM Loaded');
+    }
+}
+
+function assessState() {
+    switch (document.readyState) {
+        case 'loading':
+            document.onreadystatechange = assessState;
+            break;
+        // case 'interactive':
+        case 'complete':
+            checkGTMLoad();
+            break;
+    }
+}
+
+assessState();
