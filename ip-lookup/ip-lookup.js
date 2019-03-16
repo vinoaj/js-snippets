bd2d61c2f70076a1832d8033c0febb4c752c8888
Vinoaj Vijeyakumaar <vinoaj@gmail.com>
2019-03-17 10:41:30 +1100

    Change to ipstack.com

--- ip-lookup/ip-lookup.js
+++ ip-lookup/ip-lookup.js
@@ -3,11 +3,15 @@
  * @author vinoaj@vinoaj.com (Vinoaj Vijeyakumaar)
  */
 
+// Create your own API key at https://ipstack.com/product
+var API_KEY = '5088de54b47d56dac26bbb2b333312f0';
+var API_ENDPOINT = 'http://api.ipstack.com/check?access_key=' + API_KEY;
+
 //Load jQuery if it hasn't been already loaded
 if (window.jQuery === undefined) {
-    var s = document.createElement('script');
-    s.src = "https://code.jquery.com/jquery-3.2.1.min.js";
-    document.head.appendChild(s);
+  var s = document.createElement('script');
+  s.src = "https://code.jquery.com/jquery-3.2.1.min.js";
+  document.head.appendChild(s);
 }
 
 var ipLookup = {};
@@ -32,14 +36,12 @@ ipLookup.data = {};
  * Executes an IP data lookup for current client
  * @returns {Promise}
  */
- ipLookup.lookup = function () {
-  return new Promise(function(resolve,reject) {
-    //If no arguments are provided, the API looks up details for the IP of
-    //  originating request.
-    $.getJSON('https://freegeoip.net/json/?callback=?').done(function(data) {
+ipLookup.lookup = function () {
+  return new Promise(function (resolve, reject) {
+    $.getJSON(API_ENDPOINT).done(function (data) {
       ipLookup.data = data;
       resolve(data);
-    }).fail(function(data) {
+    }).fail(function (data) {
       reject(data);
     });
   });
